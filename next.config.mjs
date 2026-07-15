f3de3dbfd374f1fa747afb882d1b4f5266a626d3
Mason <luigixri@gmail.com>
2026-07-15 18:13:52 +0200

    Fix: serverExternalPackages per imapflow/pino

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  serverExternalPackages: ['imapflow', 'pino', 'thread-stream'],
 };
 
 export default nextConfig;
